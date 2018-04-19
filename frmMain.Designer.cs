c1f8417a4fe324421b02fc16e5f6548102c50181
sajjad javed <sajjadjaved01@gmail.com>
2018-04-19 12:00:05 +0500

    FirstCommit
    Added Files

---
+++ frmMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace frmDownTemp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblStatus.Location = new System.Drawing.Point(38, 2);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(16, 17);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "¥";
+            this.lblStatus.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar1.Location = new System.Drawing.Point(0, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(251, 25);
+            this.progressBar1.Step = 20;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(251, 32);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Updater";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace frmDownTemp
+{
+    public partial class frmMain : Form
+    {
+        FileDown down = new FileDown();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int x = Screen.PrimaryScreen.WorkingArea.Width - Width;
+            int y = Screen.PrimaryScreen.WorkingArea.Height - Height;
+            Location = new Point(x, y);
+            down.DownloadComplete += new EventHandler(Down_DownloadComplete);
+            down.ProgressChanged += new FileDown.DownloadProgressHandler(Down_ProgressChanged);
+            down.AsyncDownload("docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/d1d7081j82dhgmtgpicfi21qh0h5l41d/1476187200000/12429020242186261639/*/0B_mrqPTrlu-2NVFRaVY4cXVoVjA?e=download");
+        }
+
+        private void Down_ProgressChanged(object sender, FileDown.DownloadEventArgs e)
+        {
+            lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = e.PercentDone + "%"; }));
+            //Invoke(new MethodInvoker(delegate { Text = e.PercentDone + "%"; }));
+            progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = e.PercentDone; }));
+        }
+
+        private void Down_DownloadComplete(object sender, EventArgs e)
+        {
+            lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = "Download Completed" ; }));
+            progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = 100; }));
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //var window = MessageBox.Show("Close the window","",buttons: MessageBoxButtons.YesNo);
+            //if (window == DialogResult.No) e.Cancel = true;
+            //else e.Cancel = false;
+            if (progressBar1.Value == 100)
+            {
+                e.Cancel = false;
+            }
+            else { e.Cancel = true; }
+        }
+
+        //protected override void WndProc(ref Message message)
+        //{
+        //    if (message.Msg == SingleInstance.WM_SHOWFIRSTINSTANCE)
+        //    {
+        //        ShowWindow();
+        //    }
+        //    base.WndProc(ref message);
+        //}
+        //public void ShowWindow()
+        //{
+        //    // Insert code here to make your form show itself.
+        //    WinApi.ShowToFront(Handle);
+        //}
+    }
+}
